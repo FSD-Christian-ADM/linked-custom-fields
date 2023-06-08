7e59d773eaa329fd2a857160e8cd1dffe92b283c
Christian Schulz <christian.schulz@fsd-web.de>
2023-06-08 14:36:02 +0200

    deactivated for view_all_bugs because it caused problems in the filter overview (dependent custom-field disappeared after selecting mother option)

--- LinkedCustomFields/LinkedCustomFields.php
+++ LinkedCustomFields/LinkedCustomFields.php
@@ -1,22 +1,22 @@
 <?php
 # Copyright (c) 2011 Robert Munteanu (robert@lmn.ro)
 
-# Linked custom fields for MantisBT is free software: 
+# Linked custom fields for MantisBT is free software:
 # you can redistribute it and/or modify it under the terms of the GNU
-# General Public License as published by the Free Software Foundation, 
+# General Public License as published by the Free Software Foundation,
 # either version 2 of the License, or (at your option) any later version.
 #
-# Linked custom fields plugin for MantisBT is distributed in the hope 
-# that it will be useful, but WITHOUT ANY WARRANTY; without even the 
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# Linked custom fields plugin for MantisBT is distributed in the hope
+# that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Linked custom fields plugin for MantisBT.  
+# along with Linked custom fields plugin for MantisBT.
 # If not, see <http://www.gnu.org/licenses/>.
 
 class LinkedCustomFieldsPlugin extends MantisPlugin {
-    
+
     public function register() {
         $this->name = plugin_lang_get("title");
         $this->description = plugin_lang_get("description");
@@ -32,18 +32,18 @@ class LinkedCustomFieldsPlugin extends MantisPlugin {
         $this->contact = "robert@lmn.ro";
         $this->url ="http://www.mantisbt.org/wiki/doku.php/mantisbt:linkedcustomfields";
     }
-    
+
     public function hooks() {
         return array(
             'EVENT_LAYOUT_RESOURCES' => 'resources',
         );
     }
 
-    
+
     function resources( $p_event ) {
 
         $resources = "";
-        
+
         $t_bug_id = gpc_get_int('bug_id', -1);
         $t_m_id = gpc_get_int('m_id', 0);
         if ( $t_bug_id == -1 && basename($_SERVER['SCRIPT_NAME']) == 'bug_report_page.php' ) {
@@ -56,28 +56,29 @@ class LinkedCustomFieldsPlugin extends MantisPlugin {
         }
 
 
+        // deactivated because it caused problems in the filter overview (dependent custom-field disappeared after selecting mother option)
         if ( basename($_SERVER['SCRIPT_NAME']) == 'view_all_bug_page.php' ) {
-            $resources .= '<script type="text/javascript" src="' . plugin_page( 'view_all_bug_page_custom_field_links.php' ) . '"></script>';
+            # $resources .= '<script type="text/javascript" src="' . plugin_page( 'view_all_bug_page_custom_field_links.php' ) . '"></script>';
         }
 
 
         if ( basename($_SERVER['SCRIPT_NAME']) == 'plugin.php' ) {
             $resources .= '<script type="text/javascript" src="'.plugin_file('js/linked_custom_fields.js').'"></script>';
         }
-        
+
 
         return $resources;
 
-    }    
-    
+    }
+
     public function init() {
-        
+
         require_once 'LinkedCustomFields.API.php';
     }
-    
+
     public function schema() {
         return array(
-            array( 'CreateTableSQL', 
+            array( 'CreateTableSQL',
                 array( plugin_table( 'data' ), "
                     custom_field_id    I NOTNULL,
                     custom_field_value_order    I NOTNULL,
